09aef89c5e180be1e6f4b22324adbcd23c9d12c0
Kamil Krawiec <95848075+Kamil-Krawiec@users.noreply.github.com>
2025-07-10 22:13:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("./requirements.txt") as f:
 
 setup(
     name='intelligent-data-generator',
-    version='1.2.2',
+    version='1.2.3',
     author='Kamil Krawiec',
     author_email='kamil.krawiec9977@gmail.com',
     description='A Python package for generating semantically and syntactically correct data for RDBMS.',
